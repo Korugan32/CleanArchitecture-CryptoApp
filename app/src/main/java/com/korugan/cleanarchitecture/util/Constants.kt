8254d80311621111ad4b93cfc9af4be00fa38ace
Korugan32 <tahacansenel@gmail.com>
2024-11-14 00:22:32 +0300

    Update Constants

--- app/src/main/java/com/korugan/cleanarchitecture/util/Constants.kt
+++ app/src/main/java/com/korugan/cleanarchitecture/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.korugan.cleanarchitecture.util
 
 object Constants {
-    const val API_KEY = "apikey 5I8Q3obT0aSMn3bBmR16KG:48iKHlGYz7ulpEtW8v8qBl"
-    const val BASE_URL = "https://api.collectapi.com"
+    const val API_KEY = "yourApiKey"
+    const val BASE_URL = "https://api.collectapi.com/"
     const val CONTENT_TYPE = "application/json"
 }
