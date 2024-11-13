ca6a4cb0a927ed2fc5e1a7b52cd17763b53f3c7e
Korugan32 <tahacansenel@gmail.com>
2024-11-14 00:20:13 +0300

    Update MainActivity

--- app/src/main/java/com/korugan/cleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/korugan/cleanarchitecture/presentation/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.korugan.cleanarchitecture.presentation.crypto.CryptoScreen
 import com.korugan.cleanarchitecture.presentation.ui.theme.CleanArchitectureTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CleanArchitectureTheme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CryptoScreen(
+                        paddingValues = innerPadding
+                    )
+                }
             }
         }
     }
